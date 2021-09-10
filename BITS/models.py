71ab5c238612c18256ca436a4c293815cd58a806
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 20:26:57 +1000

    Spotify DB Intergration

--- BITS/models.py
+++ BITS/models.py
@@ -13,6 +13,7 @@ class Album(models.Model):
     release_date = models.DateField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     image = models.ImageField(null = True, blank = True, upload_to="images/")
+    Spotify_Embeded_Code = models.CharField(max_length=300)
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
@@ -22,3 +23,4 @@ class Song(models.Model):
     release_date = models.DateField()
     length_min = models.FloatField()
     producer = models.CharField(max_length=1000)
+    Spotify_Embeded_Code = models.CharField(max_length=300)
