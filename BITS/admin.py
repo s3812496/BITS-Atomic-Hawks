5505986b2809246a1d7bdfb41b6d791d16e05f48
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 12:24:26 +1000

    Start Definition of Database

--- BITS/admin.py
+++ BITS/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Song
+admin.site.register(Song)
+
+from .models import Album
+admin.site.register(Album)
+
+from .models import Artist
+admin.site.register(Artist)
--- BITS/models.py
+++ BITS/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+
+class Artist(models.Model):
+    title = models.CharField(max_length=200)
