bd13556ac3ee00b262909eb7d526b73700617684
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 13:23:14 +1000

    DB Changing order

--- BITS/models.py
+++ BITS/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Artist(models.Model):
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to="images_artist")
+    description = models.CharField(max_length=20000)
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='images_album')
+    description = models.CharField(max_length=20000)
+    release_date = models.DateField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
 class Song(models.Model):
     title = models.CharField(max_length=200)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
@@ -11,14 +23,5 @@ class Song(models.Model):
     length_min = models.FloatField()
     producer = models.CharField(max_length=1000)
 
-class Album(models.Model):
-    title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='images_album')
-    description = models.CharField(max_length=20000)
-    release_date = models.DateField()
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
-class Artist(models.Model):
-    title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to="images_artist")
-    description = models.CharField(max_length=20000)
+
