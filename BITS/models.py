d757a127a60f90f276816d25736823d7cc5ece75
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 20:09:01 +1000

    Image intergration

--- BITS/models.py
+++ BITS/models.py
@@ -5,12 +5,14 @@ from django.db import models
 class Artist(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField(null = True, blank = True, upload_to="images/")
 
 class Album(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     release_date = models.DateField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    image = models.ImageField(null = True, blank = True, upload_to="images/")
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
