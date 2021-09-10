5f34047b4b000d709738d04a913da2e2457e23d2
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 13:33:18 +1000

    remove pillow

--- BITS/models.py
+++ BITS/models.py
@@ -4,12 +4,10 @@ from django.db import models
 
 class Artist(models.Model):
     title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to="images_artist")
     description = models.CharField(max_length=20000)
 
 class Album(models.Model):
     title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='images_album')
     description = models.CharField(max_length=20000)
     release_date = models.DateField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
