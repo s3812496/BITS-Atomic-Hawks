75b318670d239c4e3470c435c2f5998b1abf202b
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 12:12:04 +1000

    Mapping app to root

--- AtmoicHawks/urls.py
+++ AtmoicHawks/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('BITS/', include('BITS.urls')),
+    path('', include('BITS.urls')),
     path('admin/', admin.site.urls),
 ]
