cd12de8516a957b98d03fe921376cfc161538f92
Nathan <s3812496@student.rmit.edu.au>
2021-09-10 13:21:13 +1000

    Adding DB Tables

--- BITS/models.py
+++ BITS/models.py
@@ -4,9 +4,21 @@ from django.db import models
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    description = models.CharField(max_length=20000)
+    release_date = models.DateField()
+    length_min = models.FloatField()
+    producer = models.CharField(max_length=1000)
 
 class Album(models.Model):
     title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='images_album')
+    description = models.CharField(max_length=20000)
+    release_date = models.DateField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
 class Artist(models.Model):
     title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to="images_artist")
+    description = models.CharField(max_length=20000)
