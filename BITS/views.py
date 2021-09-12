fa35f9ebddfb41cc94d6fa1019ee1a4e31f8e719
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 18:31:17 +1000

    Fixing varible references

--- BITS/views.py
+++ BITS/views.py
@@ -16,11 +16,11 @@ def song_by_id(request, Song_id):
 def artist_by_id(request, Artist_id):
     ABLst = Album.objects.all() # Imports all albums in to a array for later
     artist = Artist.objects.get(pk=Artist_id)
-    context=context = {'Artist':artist, 'Album' :ABLst}
+    context=context = {'Artist':artist, 'ABLst' :ABLst}
     return render(request, 'artist_by_id.html', context=context)
 
 def album_by_id(request, Album_id):
     SNGst = Song.objects.all() # Imports all Songs in to a array for later
     album = Album.objects.get(pk=Album_id)
-    context=context = {'Album':album, 'Artist' :SNGst}
+    context=context = {'Album':album, 'SNGst' :SNGst}
     return render(request, 'album_by_id.html', context=context)
