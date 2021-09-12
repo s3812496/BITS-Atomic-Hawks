10262ec32cc7d8fdfbb99aaf69c61d06a904752c
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 17:50:13 +1000

    adding artist and album refrences

--- BITS/views.py
+++ BITS/views.py
@@ -12,3 +12,15 @@ def song_by_id(request, Song_id):
     song = Song.objects.get(pk=Song_id)
     context=context = {'Song':song}
     return render(request, 'song_by_id.html', context=context)
+
+def artist_by_id(request, Artist_id):
+    ABLst = Album.objects.all() # Imports all albums in to a array for later
+    artist = Artist.objects.get(pk=Artist_id)
+    context=context = {'Artist':artist}
+    return render(request, 'artist_by_id.html', context=context)
+
+def album_by_id(request, Album_id):
+    SNGst = Song.objects.all() # Imports all Songs in to a array for later
+    album = Album.objects.get(pk=Album_id)
+    context=context = {'Album':album}
+    return render(request, 'album_by_id.html', context=context)
