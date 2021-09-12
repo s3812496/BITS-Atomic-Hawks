c633442b7b3cc2b4c55cb16f0f84958055d0ff44
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 18:02:43 +1000

    Fixing Image refernce

--- BITS/urls.py
+++ BITS/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('song/<int:Song_id>', views.song_by_id, name='song_by_id'),
     path('artist/<int:Artist_id>', views.artist_by_id, name='artist_by_id'),
     path('album/<int:Album_id>', views.album_by_id, name='album_by_id'),
+    
 ]
