67fc08f5bee0d6b116153b8b22572cd4627ba54f
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 17:54:00 +1000

    Fixing URLS Refrencing

--- BITS/urls.py
+++ BITS/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('song/<int:Song_id>', views.song_by_id, name='song_by_id'),
     path('artist/<int:Artist_id>', views.artist_by_id, name='artist_by_id'),
-    path('album/<int:Song_id>', views.album_by_id, name='album_by_id'),
+    path('album/<int:Album_id>', views.album_by_id, name='album_by_id'),
 ]
