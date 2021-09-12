1793e228f66f4eead16807aab82a4879c42ecd05
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 19:18:35 +1000

    Fix views

--- BITS/views.py
+++ BITS/views.py
@@ -4,12 +4,11 @@ from django.http import HttpResponse
 from .models import Song, Artist, Album
 
 # Create your views here.
+def contactus(request):
+    return render(request, 'contact_us.html')
 
 def index(request):
     return render(request, 'index.html')
-
-def contactus(request):
-    return render(request, 'contact_us.html')
 
 def song_by_id(request, Song_id):
     song = Song.objects.get(pk=Song_id)
