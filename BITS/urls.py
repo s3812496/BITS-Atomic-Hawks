336e04089d57bc0e5688a46b0936a5658ace3c66
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 19:21:39 +1000

    Change to URL file

--- BITS/urls.py
+++ BITS/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('song/<int:Song_id>', views.song_by_id, name='song_by_id'),
     path('artist/<int:Artist_id>', views.artist_by_id, name='artist_by_id'),
     path('album/<int:Album_id>', views.album_by_id, name='album_by_id'),
-    path('contactus/', views.index, name='contactus')
+    path('contactus/', views.index, name='contactus'),
+    path('', views.index, name='index'),
 ]
