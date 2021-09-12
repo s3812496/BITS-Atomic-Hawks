062f9a5fbca1f368a7f370bc71dabeeaba7c6ae6
Nathan <s3812496@student.rmit.edu.au>
2021-09-12 16:29:10 +1000

    Individual song id

--- BITS/views.py
+++ BITS/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Song, Artist, Album
 
 # Create your views here.
 
 def index(request):
+    return render(request, 'index.html')
+
+def song_by_id(request, Song_id):
+    Song = Song.objects.get(pk=Song_id)
     return render(request, 'index.html')
