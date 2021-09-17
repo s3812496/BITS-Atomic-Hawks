441a5ddc36a88dd8eda01ef13039aec6136475c7
Nathan <s3812496@student.rmit.edu.au>
2021-09-17 20:59:15 +1000

    Fix form

--- BITS/forms.py
+++ BITS/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField(label='E-Mail')
-    phone = forms.NumberInput()
+    phone = forms.CharField()
     body = forms.CharField(widget=forms.Textarea)
